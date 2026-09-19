b7be32b6beaae5d17ea5d4bfbd48822ddd0ca39e
Ragul <ragulvenkat30@gmail.com>
2026-09-19 18:15:59 +0530

    Initial commit with sidebar

---
+++ app/(dashboard)/projects/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Projectspage (){
+    return<div>This page shows the list of all projects associated with the loggedin user</div>
+}
+
+export default Projectspage;
